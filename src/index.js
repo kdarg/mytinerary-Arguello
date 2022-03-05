a1e3c532cdbc413a6c9fc34c36ab1bcf50acf490
Karen Arguello <karuarguello@gmail.com>
2022-03-05 15:48:30 -0300

    starting w redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import mainReducer from './redux/reducers/mainReducer';
+import thunk from 'redux-thunk';
+
+const reduxStore = createStore(mainReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
