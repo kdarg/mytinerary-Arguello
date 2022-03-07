5f8b1891f9df86974581e218eac368c0c30272c4
Karen Arguello <karuarguello@gmail.com>
2022-03-07 01:27:15 -0300

    trying redux cardcities

--- src/components/Cardscities.js
+++ src/components/Cardscities.js
@@ -1,150 +1,24 @@
-// import Button from 'react-bootstrap/Button'
-// import { Link } from "react-router-dom";
-// import axios from 'axios';
-// import React, {useEffect, useState} from 'react';
-// import Noresults from './Noresults';
-// import { connect } from "react-redux";
-// import citiesActions from "../redux/actions/citiesActions";
-
-// const Cardscities = (props) => {
-
-//     //get cities by action:
-
-//     useEffect(() => {
-//         props.getCities() 
-//     }, [])
-
-//     const inputsearch = (e) => {
-//         props.filterCities(e.target.value)
-//     }
-
-    
-//     return ( 
-//         <>
-        
-// {/* MINI PRESENTATION */}
-
-//         <div className="popular_mytineraries"><h3 className="popular_fs">Adventures ahead!</h3></div>
-
-// {/* SEARCH */}
-
-//         <div className=" d-flex justify-content-center mt-3 mb-3"> 
-//         <input type='text' placeholder='Search destination..' className='input-search' onChange={inputsearch}/> 
-//         </div>
-
-// {/* CARDS */}
-
-//         <div  className="d-flex flex-wrap justify-content-center align-items-center">
-
-//         {cities.length !== 0 ? (cities.map((city) => (
-
-//         <div key= {city._id} className="maincontainer">
-
-//         <div className="thecard">
-
-//         <div className="thefront"> 
-            
-//             <div className=" d-flex flex-column justify-content-center align-items-center polaroid-two ">
-//             <img src={process.env.PUBLIC_URL+"/assets/cities/"+ city.src} alt='City' className='image_card_size d-flex'/>
-
-//             <div className='fw-bold p-4 f-size-cardscities'>
-//                 <p>{city.city} - {city.country}</p>
-//             </div>
-
-//             </div>
-        
-//         </div>
-
-//         <div className="theback d-flex flex-column justify-content-center align-items-center"> 
-            
-//             <h1 className="text-center fs-4">{city.city} - {city.country}</h1><p>{city.description}</p>
-
-//         <Link to={`/city/${city._id}`}><Button variant='outline-dark' className='bg-button-more'>See more</Button></Link>
-//         </div>
-
-//         </div>
-
-
-//         </div>
-
-//         ))): <Noresults/> }
-
-//         </div> 
-
-//         </>
-//     );
-
-// }
-
-// const mapStateToProps = state => {
-//     return {
-//       allCities: state.citiesReducer.allCities, //va el reducer en el medio?
-//     }
-//   }
-  
-//   const mapDispatchToProps = {
-//     getCities: citiesActions.getCities,
-//     filterCities: citiesActions.filterCities
-//   }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Cardscities);
-
-
-
-
-
-
 import Button from 'react-bootstrap/Button'
 import { Link } from "react-router-dom";
-import axios from 'axios'
-import React, {useEffect, useState} from 'react'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import Noresults from './Noresults';
+import { connect } from "react-redux";
+import citiesActions from "../redux/actions/citiesActions";
 
-const Cardscities = () => {
+const Cardscities = (props) => {
 
-    const [cities, setCities] = useState([])
-    const [allMyCities, setAllMyCities] = useState([])
-    const [search, setSearch] = useState('')
-
-    const petitionGet = async () => {
-    await axios
-        .get(`http://localhost:4000/api/allcities`)
-        .then((response) => {
-            setCities(response.data.response.allcities);
-            setAllMyCities(response.data.response.allcities);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    };
-
-    const searching = (event) => {
-    setSearch(event.target.value);
-    searchFilter(event.target.value);
-    };
-
-    const searchFilter = (userSearching) => {
-    let userSearch = allMyCities.filter((info) => {
-        if (
-            info.city
-                .toString()
-                .toLowerCase()
-                .startsWith(userSearching.toLowerCase().trim())
-        ) {  
-            return info;
-        }else{
-            return //console.log('search doesnt exist') 
-        }
-    });
-
-    setCities(userSearch);
-
-    };
+    //get cities by action:
 
     useEffect(() => {
-    petitionGet();
-    }, []);
+        props.getCities() 
+    }, [])
 
+    const inputsearch = (e) => {
+        props.filterCities(e.target.value)
+    }
+
+    
     return ( 
         <>
         
@@ -155,7 +29,7 @@ const Cardscities = () => {
 {/* SEARCH */}
 
         <div className=" d-flex justify-content-center mt-3 mb-3"> 
-        <input type='text' placeholder='Search destination..' className='input-search' value={search} onChange={searching}/> 
+        <input type='text' placeholder='Search destination..' className='input-search' onChange={inputsearch}/> 
         </div>
 
 {/* CARDS */}
@@ -199,6 +73,132 @@ const Cardscities = () => {
 
         </>
     );
+
 }
 
-export default Cardscities;
+const mapStateToProps = state => {
+    return {
+      allCities: state.citiesReducer.allCities, //va el reducer en el medio?
+    }
+  }
+  
+  const mapDispatchToProps = {
+    getCities: citiesActions.getCities,
+    filterCities: citiesActions.filterCities
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cardscities);
+
+
+
+
+
+
+// import Button from 'react-bootstrap/Button'
+// import { Link } from "react-router-dom";
+// import axios from 'axios'
+// import React, {useEffect, useState} from 'react'
+// import Noresults from './Noresults';
+
+// const Cardscities = () => {
+
+//     const [cities, setCities] = useState([])
+//     const [allMyCities, setAllMyCities] = useState([])
+//     const [search, setSearch] = useState('')
+
+//     const petitionGet = async () => {
+//     await axios
+//         .get(`http://localhost:4000/api/allcities`)
+//         .then((response) => {
+//             setCities(response.data.response.allcities);
+//             setAllMyCities(response.data.response.allcities);
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+//     };
+
+//     const searching = (event) => {
+//     setSearch(event.target.value);
+//     searchFilter(event.target.value);
+//     };
+
+//     const searchFilter = (userSearching) => {
+//     let userSearch = allMyCities.filter((info) => {
+//         if (
+//             info.city
+//                 .toString()
+//                 .toLowerCase()
+//                 .startsWith(userSearching.toLowerCase().trim())
+//         ) {  
+//             return info;
+//         }else{
+//             return //console.log('search doesnt exist') 
+//         }
+//     });
+
+//     setCities(userSearch);
+
+//     };
+
+//     useEffect(() => {
+//     petitionGet();
+//     }, []);
+
+//     return ( 
+//         <>
+        
+// {/* MINI PRESENTATION */}
+
+//         <div className="popular_mytineraries"><h3 className="popular_fs">Adventures ahead!</h3></div>
+
+// {/* SEARCH */}
+
+//         <div className=" d-flex justify-content-center mt-3 mb-3"> 
+//         <input type='text' placeholder='Search destination..' className='input-search' value={search} onChange={searching}/> 
+//         </div>
+
+// {/* CARDS */}
+
+//         <div  className="d-flex flex-wrap justify-content-center align-items-center">
+
+//         {cities.length !== 0 ? (cities.map((city) => (
+
+//         <div key= {city._id} className="maincontainer">
+
+//         <div className="thecard">
+
+//         <div className="thefront"> 
+            
+//             <div className=" d-flex flex-column justify-content-center align-items-center polaroid-two ">
+//             <img src={process.env.PUBLIC_URL+"/assets/cities/"+ city.src} alt='City' className='image_card_size d-flex'/>
+
+//             <div className='fw-bold p-4 f-size-cardscities'>
+//                 <p>{city.city} - {city.country}</p>
+//             </div>
+
+//             </div>
+        
+//         </div>
+
+//         <div className="theback d-flex flex-column justify-content-center align-items-center"> 
+            
+//             <h1 className="text-center fs-4">{city.city} - {city.country}</h1><p>{city.description}</p>
+
+//         <Link to={`/city/${city._id}`}><Button variant='outline-dark' className='bg-button-more'>See more</Button></Link>
+//         </div>
+
+//         </div>
+
+
+//         </div>
+
+//         ))): <Noresults/> }
+
+//         </div> 
+
+//         </>
+//     );
+// }
+
+// export default Cardscities;
